f36cbc9f5fad40d9144df132449bd1386be814fe
Yondbox <81684629+yondbox@users.noreply.github.com>
2021-04-03 15:54:52 +0000

    fix: Module parse failed: Unexpected token
    You may need an appropriate loader to handle this file type,
    currently no loaders are configured to process this file.
    See https://webpack.js.org/concepts#loaders

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\\.(ts|tsx)$/,
+                test: /\.(ts|tsx)$/,
                 loader: 'ts-loader',
                 exclude: ['/node_modules/'],
             },
